2accdf593f94d34c7d2c44a284e30afd9a437b2c
Madhusudan Aritakula <madhusudan.aritakula@Madhusudans-MacBook-Pro.local>
2024-10-12 14:25:00 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
+Video: https://www.youtube.com/watch?v=NgayZAuTgwM
 ## Getting Started
 
 First, run the development server:
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,14 @@
+"use client"
+type TodoItemProps={
+    id: string 
+    title: string 
+    complete: boolean
+    toggleTodo: (id: string, complete:boolean)=> void
+}
+export function TodoItem({id, title, complete,toggleTodo}:TodoItemProps){
+   return <li className="flex gap-1 items-center">
+    <input id={id} type="checkbox" className="cursor-ponter peer" defaultChecked={complete}
+    onChange={e => toggleTodo(id, e.target.checked)}/>
+    <label htmlFor={id} className="peer-checked:line-through">{title}</label>
+   </li> 
+}
